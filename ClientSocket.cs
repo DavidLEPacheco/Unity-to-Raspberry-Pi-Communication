9d3349f3423d4629ea1f06b0071e830323a3fffc
DavidLEPacheco <l.pacheco@outlook.com>
2022-07-23 16:23:20 +1000

    Add files via upload
    Added code to receive data from python server

--- ClientSocket.cs
+++ ClientSocket.cs
@@ -1,4 +1,4 @@
-﻿//Client socket. Initialize after Server socket is generated
+//Client socket. Initialize after Server socket is generated
 
 using UnityEngine;
 using System.Collections;
@@ -16,7 +16,7 @@ public class ClientSocket : MonoBehaviour
     public NetworkStream theStream;
     StreamWriter theWriter;
     StreamReader theReader;
-    public String Host = "192.168.1.7";
+    public String Host = "192.168.1.103";
     public Int32 Port = 12345;
 
     void Start()
@@ -24,12 +24,19 @@ public class ClientSocket : MonoBehaviour
         setupSocket(); //this code sets up the socket AND connects to the server
         Debug.Log("Socket set up");
 
-        Byte[] sendBytes = Encoding.UTF8.GetBytes("This is the client"); //THIS WORKED. FIND OUT HOW
-        theStream.Write(sendBytes, 0, sendBytes.Length); //THIS WORKED. FIND OUT HOW
+        Byte[] sendBytes = Encoding.UTF8.GetBytes("This is the client"); //Encodes message in a UTF8 byte format to allow it to send over socket
+        theStream.Write(sendBytes, 0, sendBytes.Length); //Send message to server with offset and length
 
-        theWriter.WriteLine("This is the client"); //THIS DOESN'T WORK. FIND OUT WHY
-        theWriter.Write(sendBytes); //THIS DOESN'T WORK. FIND OUT WHY
         Debug.Log("Sent message. Waiting for response");
+
+        //Read response from server
+        Byte[] readBytes = new byte[1024];
+        StringBuilder myCompleteMessage = new StringBuilder();
+        int numberOfBytesRead = 0;
+        numberOfBytesRead = theStream.Read(readBytes, 0, readBytes.Length);
+        myCompleteMessage.AppendFormat("{0}", Encoding.ASCII.GetString(readBytes, 0, numberOfBytesRead));
+        Debug.Log("You received the following message: " + myCompleteMessage);
+
     }
 
 
