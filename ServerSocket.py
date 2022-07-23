f501fae18bf93763c450da4fbb47ab6ce8d331b3
DavidLEPacheco <l.pacheco@outlook.com>
2022-07-23 07:29:21 +0100

    Add files via upload
    Fixed response code to client

--- ServerSocket.py
+++ ServerSocket.py
@@ -1,14 +1,14 @@
 from __future__ import print_function #allows print functions to work as they do in python 3 for consistency (prevents paranthesis printing to the console)
 import socket
 import sys
-from blinkled import blinkled
+#from blinkled import blinkled
 
 backlog = 1
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('192.168.1.7', 12345)) #bind creates one unique address which is built up from both the ip address and port number.
+s.bind(('192.168.1.109', 12345)) #bind creates one unique address which is built up from both the ip address and port number.
 s.listen(1)
-blinkled()
+#blinkled()
 
 
 while True:
@@ -17,7 +17,8 @@ while True:
     print ('Connection from', client_address)
     data = connection.recv(1024)
     print ('Received "%s"' % data) #This works and receives UTF8 encoded data
-    connection.sendall("This is the server")
+    connection.sendall(b'This is the server')
+    print ('Sent acknowledgement to client')
 
 try: #this block of code isn't working for some reason. The client is being accepted but it's not printing 'Connection  from'. Maybe because no data is being received.
     print ('Connection from', client_address)
